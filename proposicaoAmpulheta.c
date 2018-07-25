8db7526f7053c5605926a965f6504454b9f02f0d
Murilo Borges de Andrade <mandrade@t540p.local>
2018-07-24 22:54:20 -0300

    fix: adjustment of countHourglass

--- proposicaoAmpulheta.c
+++ proposicaoAmpulheta.c
@@ -5,19 +5,22 @@
 #define NUM_ROWS(a) ARRAYSIZE(a)
 #define NUM_COLS(a) ARRAYSIZE(a[0])
 
-#define INITIAL_ROW 6
-#define INITIAL_COL 6
+#define INITIAL_ROW 9
+#define INITIAL_COL 9
 #define MAGICAL_NUMBER 3
 #define INITIAL_RESULT_ROW INITIAL_ROW / MAGICAL_NUMBER
 #define INITIAL_RESULT_COL INITIAL_COL / MAGICAL_NUMBER
 
 int matrix[INITIAL_ROW][INITIAL_COL] = {
-{1, 2, 2, 0, 0, 0},
-{0, 1, 0, 0, 0, 0},
-{2, 2, 2, 0, 0, 0},
-{0, 0, 0, 2, 2, 2},
-{0, 0, 0, 0, 3, 0},
-{0, 0, 0, 2, 2, 2}
+{-8, 5, -2, 1, 2, 2, 0, 0, 0},
+{-8, 5, -1, 0, 1, 0, 0, 0, 0},
+{-8, 2, -3, 2, 2, 2, 0, 0, 0},
+{-7, 5, -2, 0, 0, 0, 2, 2, 2},
+{-6, 2, -1, 0, 0, 0, 0, 3, 0},
+{-8, 5, -1, 0, 0, 0, 2, 2, 2},
+{-1, 5, -2, 0, 0, 0, 2, 2, 2},
+{-8, 0, -1, 0, 0, 0, 2, 2, 2},
+{-4, 5, -3, 0, 0, 0, 2, 2, 2}
 };
 
 void printMatrix(int (*matrix)[INITIAL_COL], int rows, int cols) {
@@ -52,13 +55,18 @@ void resetDynamicMatrix(int *matrix, int rows, int cols) {
 
 void countHourglass(int (*matrix)[INITIAL_COL], int rows, int cols, int *matrixResult) {
 	int i = 0, j = 0, offset = 0;
+	int xi = 0, yj = 0;
         for (int x = 0; x < rows; x++) {
 		i = x / MAGICAL_NUMBER;
                 for (int y = 0; y < cols; y++) {
 			j = y / MAGICAL_NUMBER;
-			//printf(" i = %d , j = %d  || x = %d , y = %d \n", i, j, x, y);
-			offset = i * INITIAL_RESULT_COL + j;
+			xi = x - (i * MAGICAL_NUMBER);
+			yj = y - (j * MAGICAL_NUMBER);
+//			printf(" xi = %d , yj = %d \n", x - (i * MAGICAL_NUMBER), y - (j * MAGICAL_NUMBER));
+			if (xi == 1 && (yj == 0 || yj == 2 ))
+				continue;
 
+			offset = i * INITIAL_RESULT_COL + j;
 			matrixResult[offset] += matrix[x][y];
 		}
 	}
